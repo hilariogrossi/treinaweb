cd48a1416de36ddb704f60fef37cccfb7c4e41e1
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2022-10-13 15:31:17 -0300

    Atualização 13/10/2022 15:31

--- frontend/Lixo/Lista.tsx
+++ frontend/Lixo/Lista.tsx
@@ -38,3 +38,55 @@
 // };
 
 // export default Lista;
+
+
+
+
+
+
+
+
+
+// import { Button } from "@mui/material";
+// import { Descricao, Foto, Informacoes, ItemLista, ListaStyled, Nome, Valor, ListaVazia } from "./Lista.style";
+// import { Professor } from "../../@types/professor";
+// import { FormatadorService } from "../../services/FormatadorService";
+
+// interface ListaProps {
+//     professores: Professor[],
+//     onSelect: (professor: Professor) => void,
+// };
+
+// const Lista = (props: ListaProps) => {
+//     //    const [contador, setContador] = useState(0);
+
+//     return (
+//         <div>
+//             {/* <div>Contador igual: {contador}.</div>
+//             <button onClick={() => setContador(contador + 1)}>Somar +1</button> */}
+
+//             {props.professores.length > 0 ? (
+//                 <ListaStyled>
+//                     {props.professores.map(professor => (
+//                         <ItemLista key={professor.id}>
+//                             <Foto src={professor.foto}>
+//                             </Foto>
+
+//                             <Informacoes>
+//                                 <Nome>{professor.nome}</Nome>
+//                                 <Valor>{FormatadorService.valorMonetario(professor.valor_hora)} por hora</Valor>
+//                                 <Descricao>{FormatadorService.limitarTexto(professor.descricao, 100)}</Descricao>
+//                                 <Button onClick={() => props.onSelect(professor)} sx={{ width: '70%' }}>Invista em você. Marcar um horário com o {professor.nome}.</Button>
+//                             </Informacoes>
+//                         </ItemLista>
+//                     ))}
+//                 </ListaStyled>
+//             ) : (
+//                 <ListaVazia>Nenhum item econtrado!</ListaVazia>
+//             )}
+
+//         </div>
+//     );
+// };
+
+// export default Lista;
--- frontend/Lixo/useIndex.ts
+++ frontend/Lixo/useIndex.ts
@@ -1,43 +1,116 @@
-import { useState } from "react";
-import { Professor } from "../../@types/professor";
+// import { useState } from "react";
+// import { Professor } from "../../@types/professor";
 
-export function useIndex() {
-    const [listaProfessores, setListaProfessores] = useState<Professor[]>([
-            {
-                id: 1,
-                nome: 'Professor 1',
-                foto: 'https://github.com/hilariogrossi.png',
-                descricao: 'Descrição do professor 1',
-                valor_hora: 250,
-            },
+// export function useIndex() {
+//     const [listaProfessores, setListaProfessores] = useState<Professor[]>([
+//             {
+//                 id: 1,
+//                 nome: 'Professor 1',
+//                 foto: 'https://github.com/hilariogrossi.png',
+//                 descricao: 'Descrição do professor 1',
+//                 valor_hora: 250,
+//             },
 
-            {
-                id: 2,
-                nome: 'Professor 2',
-                foto: 'https://github.com/hilariogrossi.png',
-                descricao: 'Descrição do professor 2',
-                valor_hora: 150,
-            },
+//             {
+//                 id: 2,
+//                 nome: 'Professor 2',
+//                 foto: 'https://github.com/hilariogrossi.png',
+//                 descricao: 'Descrição do professor 2',
+//                 valor_hora: 150,
+//             },
 
-            {
-                id: 3,
-                nome: 'Professor 3',
-                foto: 'https://github.com/hilariogrossi.png',
-                descricao: 'Descrição do professor 3',
-                valor_hora: 100,
-            },
+//             {
+//                 id: 3,
+//                 nome: 'Professor 3',
+//                 foto: 'https://github.com/hilariogrossi.png',
+//                 descricao: 'Descrição do professor 3',
+//                 valor_hora: 100,
+//             },
 
-            {
-                id: 4,
-                nome: 'Professor 4',
-                foto: 'https://github.com/hilariogrossi.png',
-                descricao: 'Descrição do professor 4',
-                valor_hora: 200,
-            },
+//             {
+//                 id: 4,
+//                 nome: 'Professor 4',
+//                 foto: 'https://github.com/hilariogrossi.png',
+//                 descricao: 'Descrição do professor 4',
+//                 valor_hora: 200,
+//             },
 
-    ]);
+//     ]);
 
-    return {
-        listaProfessores,
-    }
-};
+//     return {
+//         listaProfessores,
+//     }
+// };
+
+
+
+
+
+// import { useEffect, useState } from "react";
+// import { Professor } from "../../@types/professor";
+// import { ApiService } from "../../services/ApiService";
+
+// export function useIndex() {
+//     const [listaProfessores, setListaProfessores] = useState<Professor[]>([]);
+
+//     const [nome, setNome] = useState('');
+//     const [email, setEmail] = useState('');
+
+//     const [professorSelecionado, setProfessorSelecionado] = useState<Professor | null>(null);
+
+//     const [mensagem, setMensagem] = useState('');
+
+//     useEffect(() => {
+//         ApiService.get('/professores')
+//             .then((resposta) => {
+//                 setListaProfessores(resposta.data)
+//             })
+//     }, []); // [] o array vazio executa essa função apenas uma vez.
+
+//     useEffect(() => {
+//         limparFormulario();
+//     }, [professorSelecionado]);
+
+//     function marcarAula() {
+//         if (professorSelecionado !== null) {
+//             if (validarDadosAula()) {
+//                 ApiService.post('/professores/' + professorSelecionado.id + '/aulas', {
+//                     nome,
+//                     email
+
+//                 }).then(() => {
+//                     setProfessorSelecionado(null);
+//                     setMensagem('Cadastrado com sucesso!');
+
+//                 }).catch((error) => {
+//                     setMensagem(error.response?.data.message);
+//                 });
+
+//             } else {
+//                 setMensagem('Preencha os dados corretamente!');
+//             }
+//         }
+//     }
+
+//     function validarDadosAula() {
+//         return nome.length > 0 && email.length > 0;
+//     }
+
+//     function limparFormulario() {
+//         setNome('');
+//         setEmail('');
+//     }
+
+//     return {
+//         listaProfessores,
+//         nome,
+//         setNome,
+//         email,
+//         setEmail,
+//         professorSelecionado,
+//         setProfessorSelecionado,
+//         marcarAula,
+//         mensagem,
+//         setMensagem,
+//     }
+// };
